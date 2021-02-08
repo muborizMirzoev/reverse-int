37829be0d2bd30171297bda372638d5f70b10f38
muborizMirzoev <muboriz10@gmail.com>
2021-02-09 06:27:25 +0700

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('');
+
+  if (arr.includes('-')) {
+    return arr.slice(1, arr.legth).reverse().join('')
+  }
+  return arr.reverse().join('')
 }
